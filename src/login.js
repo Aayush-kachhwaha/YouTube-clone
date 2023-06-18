4fb9b4f5cde9d566ed71bf812e5d36c90cb886cb
Aayush Kachhwaha <aayushk2609@gmail.com>
2023-06-18 21:33:22 +0530

    update

---
+++ src/login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './login.css';
+
+function login() {
+  return (
+    <div>
+        <h2> hi my name is aayush</h2>
+        <nav className='navigation'>
+            <a href='#'>Home</a>
+            <a href='#About'>About</a>
+            <a href='#Services'>Services</a>
+            <a href='#Contact'>Contact</a>
+        </nav>
+    </div>
+  )
+}
+
+export default login
